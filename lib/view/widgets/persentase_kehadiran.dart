0c4143503631a83322f52c6d2af8fd22c739de95
RezaPtama <resapratama36@gmail.com>
2023-06-21 19:02:42 +0700

    Update Page Riwayat

--- lib/view/widgets/persentase_kehadiran.dart
+++ lib/view/widgets/persentase_kehadiran.dart
@@ -1,17 +1,20 @@
-import 'dart:js_interop';
-
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:presencee/model/riwayat_kehadiran_model.dart';
 import 'package:presencee/theme/constant.dart';
+import 'package:presencee/view/pages/history_view.dart';
 import 'package:presencee/view/widgets/State_Status_widget.dart';
 import 'package:presencee/view_model/kehadiran_view_model.dart';
 import 'package:provider/provider.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+
 
 class PersentaseKehadiran extends StatefulWidget {
   final bool diagram;
   final int selectedIndex;
-  const PersentaseKehadiran({super.key, required this.diagram, required this.selectedIndex});
+  const PersentaseKehadiran(
+      {super.key, required this.diagram, required this.selectedIndex});
 
   @override
   State<PersentaseKehadiran> createState() => _PersentaseKehadiranState();
@@ -19,8 +22,28 @@ class PersentaseKehadiran extends StatefulWidget {
 
 class _PersentaseKehadiranState extends State<PersentaseKehadiran> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     final manager = Provider.of<KehadiranViewModel>(context);
+    final hadir = manager.kehadiranNew.meta!.toJson().values.toList().map((e) => e["Hadir"]).elementAt(widget.selectedIndex);
+    final alpa = manager.kehadiranNew.meta!.toJson().values.toList().map((e) => e["Alpa"]).elementAt(widget.selectedIndex);
+    final sakit = manager.kehadiranNew.meta!.toJson().values.toList().map((e) => e["Sakit"]).elementAt(widget.selectedIndex);
+    final izin = manager.kehadiranNew.meta!.toJson().values.toList().map((e) => e["Izin"]).elementAt(widget.selectedIndex);
+    final dispen = manager.kehadiranNew.meta!.toJson().values.toList().map((e) => e["Dispensasi"]).elementAt(widget.selectedIndex);
+    var total = manager.kehadiranNew.meta!.toJson().values.toList().map((e) => e["Total"]).elementAt(widget.selectedIndex);
+
+    List<DataKehadiran> kehadiran = [
+      DataKehadiran("Hadir", hadir, "Hadir", AppTheme.primaryTheme),
+      DataKehadiran("Alpa", alpa, "Alpa", AppTheme.absen),
+      DataKehadiran("Sakit", sakit, "Sakit", AppTheme.absen),
+      DataKehadiran("Izin", izin, "Izin", AppTheme.absen),
+      DataKehadiran("Dispensasi", dispen, "Dispensasi", AppTheme.absen),
+      DataKehadiran("", total > 16 ? 0 : total - 16, "Kosong", AppTheme.kosong),
+    ];
 
     if (manager.state == DataState.initial) {
       return const LoadingMatkulCard();
@@ -30,22 +53,25 @@ class _PersentaseKehadiranState extends State<PersentaseKehadiran> {
       return const ErrorMatkulCards();
     }
 
-    getPercent1(){
-      double percent = manager.kehadiranNew.meta!.toJson().values.toList().map((e) => e["Total"]).reduce((value, element) => (value + element)) / 80 * 100;
+    getPercent1() {
+      double percent = manager.kehadiranNew.meta!.toJson().values.toList().map((e) => e["Total"]).reduce((value, element) => (value + element)) / 64 * 100;
       return percent.toStringAsFixed(percent.truncateToDouble() == percent ? 0 : 2);
     }
-    getPercent2(){
-      double percent = manager.kehadiranNew.meta!.toJson().values.toList().map((e) => e["Total"]).elementAt(widget.selectedIndex) / 16 * 100;
-      return percent.toStringAsFixed(percent.truncateToDouble() == percent ? 0 : 2);
+
+    getPercent2() {
+      double percent = manager.kehadiranNew.meta!.toJson().values.toList().map((e) => e["Total"]).elementAt(widget.selectedIndex) / 17 * 100;
+      return percent
+          .toStringAsFixed(percent.truncateToDouble() == percent ? 0 : 2);
     }
 
     return widget.diagram == false
         ? CircularPercentIndicator(
+          startAngle: 90,
             radius: 105.0,
             animation: true,
             animationDuration: 1200,
             lineWidth: 15.0,
-            percent: manager.kehadiranNew.meta!.toJson().values.toList().map((e) => e["Total"]).reduce((value, element) => (value + element)).toDouble() / 80,
+            percent: manager.kehadiranNew.meta!.toJson().values.toList().map((e) => e["Total"]).reduce((value, element) => (value + element)) / 64,
             center: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -82,22 +108,55 @@ class _PersentaseKehadiranState extends State<PersentaseKehadiran> {
             backgroundColor: AppTheme.gray,
             progressColor: AppTheme.primaryTheme,
           )
-        : CircularPercentIndicator(
-            radius: 120.0,
-            animation: true,
-            animationDuration: 1200,
-            lineWidth: 30.0,
-            percent: manager.kehadiranNew.meta!.toJson().values.toList().map((e) => e["Total"]).elementAt(widget.selectedIndex) / 16,
-            center: Text(
-              '${getPercent2()}%',
-              style: AppTextStyle.poppinsTextStyle(
-                color: AppTheme.black,
-                fontSize: 40,
-                fontsWeight: FontWeight.w700,
-              ),
-            ),
-            backgroundColor: AppTheme.gray,
-            progressColor: AppTheme.primaryTheme,
-          );
+        : SfCircularChart(
+          annotations: <CircularChartAnnotation>[
+               CircularChartAnnotation(
+                  widget: Text(
+                    '${getPercent2()}%',
+                    style: AppTextStyle.poppinsTextStyle(
+                      color: AppTheme.black,
+                      fontSize: 40,
+                      fontsWeight: FontWeight.w700
+                    ),
+
+                  )
+                ),
+            ],
+            tooltipBehavior: TooltipBehavior(enable: true),
+            series: <DoughnutSeries<DataKehadiran, String>>[
+                DoughnutSeries<DataKehadiran, String>(
+                  radius: '150',
+                  innerRadius: '110',
+                  emptyPointSettings:
+                      EmptyPointSettings(color: Colors.grey, borderWidth: 20),
+                  pointColorMapper: (datum, index) => datum.color,
+                  startAngle: 90,
+                  endAngle: 450,
+                  explode: true,
+                  // explodeIndex: 0,
+                  dataSource: kehadiran,
+                  xValueMapper: (DataKehadiran data, index) => data.xData,
+                  yValueMapper: (DataKehadiran data, index) => data.yData,
+                  // dataLabelMapper: (DataKehadiran data, _) => data.text,
+                  // dataLabelSettings: DataLabelSettings(isVisible: true)
+                ),
+              ]);
+    // : CircularPercentIndicator(
+    //     radius: 120.0,
+    //     animation: true,
+    //     animationDuration: 1200,
+    //     lineWidth: 30.0,
+    //     percent: manager.kehadiranNew.meta!.toJson().values.toList().map((e) => e["Total"]).elementAt(widget.selectedIndex) / 16,
+    //     center: Text(
+    //       '${getPercent2()}%',
+    //       style: AppTextStyle.poppinsTextStyle(
+    //         color: AppTheme.black,
+    //         fontSize: 40,
+    //         fontsWeight: FontWeight.w700,
+    //       ),
+    //     ),
+    //     backgroundColor: AppTheme.gray,
+    //     progressColor: AppTheme.primaryTheme,
+    //   );
   }
 }
